b493230a33d311e0168438e46c4397160a760b36
Basit1-a <144469928+Basit1-a@users.noreply.github.com>
2023-11-29 09:35:22 -0500

    Update TempratureSensor.cpp

--- TempratureSensor.cpp
+++ TempratureSensor.cpp
@@ -6,14 +6,17 @@
 
 TemperatureSensor::TemperatureSensor() : currentTemperature(0.0) {}
 
+// function to get current rainfall
 float TemperatureSensor::getTemperature() const {
     return currentTemperature;
 }
 
+// function to set current rainfall
 void TemperatureSensor::setTemperature(float temperature) {
     currentTemperature = temperature;
 }
 
+// function to display rainfall
 void TemperatureSensor::displayTemperature() const {
     std::cout << "Current temperature: " << std::fixed << std::setprecision(2)
         << currentTemperature << " degrees Celsius." << std::endl;
