fb1d7a02bf5854d07f13bfa42cbb88148847abfb
abenezerbrh <144065296+abenezerbrh@users.noreply.github.com>
2023-12-06 18:04:21 -0500

    Update RainfallGauge.cpp

--- RainfallGauge.cpp
+++ RainfallGauge.cpp
@@ -2,20 +2,25 @@
 #include <iostream>
 #include <iomanip>
 
+//constructor intializing currentRainfall to zero
 RainfallGauge::RainfallGauge() : currentRainfall(0.0) {}
 
+//Getter method to retrieve the current rainfall value
 float RainfallGauge::getRainfall() const {
     return currentRainfall;
 }
 
+//setter method to set the rainfall value
 void RainfallGauge::setRainfall(float rainfall) {
     currentRainfall = rainfall;
 }
 
+//method to reset the current rainfall value to zero
 void RainfallGauge::resetRainfall() {
     currentRainfall = 0.0;
 }
 
+//method to display the current rainfall value
 void RainfallGauge::displayRainfall() const {
     std::cout << "Current rainfall: " << std::fixed << std::setprecision(2)
         << currentRainfall << " mm." << std::endl;
