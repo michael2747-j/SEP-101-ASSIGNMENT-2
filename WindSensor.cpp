f343bc2e304b75a37b09bb4da33d6865b36361f1
abenezerbrh <144065296+abenezerbrh@users.noreply.github.com>
2023-12-06 18:11:48 -0500

    Update WindSensor.cpp

--- WindSensor.cpp
+++ WindSensor.cpp
@@ -3,16 +3,20 @@
 #include <iostream>
 #include <iomanip>
 
+//constructor intializing currentWinspeed to zero
 WindSensor::WindSensor() : currentWindSpeed(0.0) {}
 
+//getter method to retrieve the currentWindSpeed value
 float WindSensor::getWindSpeed() const {
     return currentWindSpeed;
 }
 
+//setter method to set the windSpeed value
 void WindSensor::setWindSpeed(float windSpeed) {
     currentWindSpeed = windSpeed;
 }
 
+//method to display the current wind speed value
 void WindSensor::displayWindSpeed() const {
     std::cout << "Current wind speed: " << std::fixed << std::setprecision(2)
         << currentWindSpeed << " km/h." << std::endl;
