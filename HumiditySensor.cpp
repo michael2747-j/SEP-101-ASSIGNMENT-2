cd64c7cdbe1d0d3a2d54385c4b8a61d2bac416a8
abenezerbrh <144065296+abenezerbrh@users.noreply.github.com>
2023-12-06 18:00:39 -0500

    Update HumiditySensor.cpp

--- HumiditySensor.cpp
+++ HumiditySensor.cpp
@@ -1,17 +1,21 @@
 #include "HumiditySensor.h"
 #include <iostream>
 
+//constructor initializing currentHumidity to zero
 HumiditySensor::HumiditySensor() : currentHumidity(0) {}
 
+//Getter method to retrieve the current humidity value
 int HumiditySensor::getHumidity() const {
     return currentHumidity;
 }
 
+//setter method to retrieve the current humidity value 
 void HumiditySensor::setHumidity(int humidity) {
     currentHumidity = humidity;
 }
 
+//method to display the current humidity value
 void HumiditySensor::displayHumidity() const {
     std::cout << "Current humidity: " << currentHumidity << "%" << std::endl;
-   
+
 }
